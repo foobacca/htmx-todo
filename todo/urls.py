5c458e5a2ce53a444db56ea267e1aa1fb5db7ce6
Hamish Downer <hamish@foobacca.co.uk>
2024-11-30 21:08:42 +0000

    basic read only views

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 
+app_name = "todo"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("list/", views.todo_list, name="list"),
+    path("<int:todo_id>/", views.todo_detail, name="detail"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.template.response import TemplateResponse
+
+from .models import TodoItem
 
 
 def index(request):
-    return HttpResponse("<html><body>Started the todo app</body></html>")
+    return TemplateResponse(request, "todo/index.html", {})
+
+
+def todo_list(request):
+    context = {
+        "todo_count": TodoItem.objects.count(),
+        "todo_list": TodoItem.objects.all(),
+    }
+    return TemplateResponse(request, "todo/list.html", context)
+
+
+def todo_detail(request, todo_id: int):
+    todo = get_object_or_404(TodoItem, id=todo_id)
+    return TemplateResponse(request, "todo/detail.html", {"todo": todo})
